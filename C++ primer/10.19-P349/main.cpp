8f67be4bed5fc3b2adf1b080ae93257cce0ea129
wuxiangdong <w15607911028@outlook.com>
2015-10-23 12:59:33 +0800

    Update

--- C++ primer/10.19-P349/main.cpp
+++ C++ primer/10.19-P349/main.cpp
@@ -6,7 +6,6 @@ using namespace std;
 void biggies(std::vector<string> &words,vector<string>::size_type sz) {
 	auto beginning = stable_partition(words.begin(), words.end(), [sz](const string &s1){return s1.size() < sz;});
 	for_each(beginning, words.end(), [](const string &s1){cout << s1 << " ";});
-
 }
 int main(int argc, char const *argv[]) {
 	std::vector<string> v{"ss","12345678","abcdef","sss","ssssss"};
